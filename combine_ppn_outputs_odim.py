d5e4a48410c23ea3f02be81f5d4d400e16c71a05
Tuuli Perttula <tuuli.perttula@fmi.fi>
2021-06-11 07:57:14 +0300

    Opening of f1 h5py file object only once

--- combine_ppn_outputs_odim.py
+++ combine_ppn_outputs_odim.py
@@ -6,7 +6,7 @@ import numpy as np
 # Tuuli Perttula 2020
 # tuuli.perttula@fmi.fi
 
-def combine_fmippn_h5_files(file1,file2):
+def combine_fmippn_h5_files(f1,f2):
     """ This function combines two FMIPPN HDF5 ODIM format output files.
     Ensemble members from file2 are added to file1. The names of 
     ensemble members from file2 are modified so that the numbering continues 
@@ -19,12 +19,13 @@ def combine_fmippn_h5_files(file1,file2):
     same except the seed value. Note that the seed attributes are not modified.
 
     Keyword arguments:
-    file1 -- FMIPPN ODIM HDF5-file to which we add members
-    file2 -- FMIPPN ODIM HDF5-file from which we copy the members to file1
+    f1 -- h5py file object for FMIPPN ODIM HDF5-file to which we copy the members from file 2 f1=h5py.File(file1, 'a')
+    f2 -- h5py file object for FMIPPN ODIM HDF5-file from which we copy the members to file1 f2=h5py.File(file2, 'r')
     """
 
-    f1=h5py.File(file1, 'a')
-    f2=h5py.File(file2, 'r')
+    #Moved outside of the function to save time
+    #f1=h5py.File(file1, 'a')
+    #f2=h5py.File(file2, 'r')
     
     # Read ensemble size from attributes
     ensemble_size_file1=int(f1['/how'].attrs.__getitem__('ensemble_size'))
@@ -35,19 +36,14 @@ def combine_fmippn_h5_files(file1,file2):
     nowcast_timestep_file2 = int(f2['/how'].attrs.__getitem__('nowcast_timestep'))
     max_leadtime_file2 = int(f2['/how'].attrs.__getitem__('max_leadtime'))
     num_timesteps_file2=int(max_leadtime_file2/nowcast_timestep_file2)
-    
-    print(ensemble_size_file1,ensemble_size_file2,num_timesteps_file2)
 
     for member in range(1,ensemble_size_file2+1):
         for leadtime in range(1,num_timesteps_file2+1):
 
-            #dset_name="/member-" + f"{member:02d}" + "/leadtime-" + f"{leadtime:02d}"
             dset_name=f"/dataset{leadtime}/data{member}/data"
             dset_what_group=f"/dataset{leadtime}/data{member}/what"
             
             new_member=member+ensemble_size_file1
-
-            #new_dset_name="/member-" + f"{new_member:02d}" + "/leadtime-" + f"{leadtime:02d}"
             new_dset_name=f"/dataset{leadtime}/data{new_member}/data"
             new_dset_what_group=f"/dataset{leadtime}/data{new_member}/what"
             
@@ -64,12 +60,11 @@ def combine_fmippn_h5_files(file1,file2):
     #Give new value to attribute meta/configuration/ENSEMBLE size 
     new_ensemble_size=(ensemble_size_file1+ensemble_size_file2)
     print('new_ensemble_size', new_ensemble_size)
-    #f1['/how'].attrs.modify('ensemble_size',np.string_([str(new_ensemble_size)]))
     f1['/how'].attrs.modify('ensemble_size',new_ensemble_size)
-            
-    f1.close()
-    f2.close()
 
+    #Moved outside of the function to save time
+    #f1.close()
+    #f2.close()
     
 def main():    
 
@@ -79,12 +74,19 @@ def main():
     cp_command='cp ' + options.inputfilelist[0] + ' ' + options.outfile
     os.system(cp_command)
 
-    #Append file1 with ensemble members from rest of the input files   
+    #Append file1 with ensemble members from rest of the input files
     if len(options.inputfilelist) >= 2:
-        for inputfile in options.inputfilelist[1:]:
-            combine_fmippn_h5_files(options.outfile,inputfile)
 
-    
+        f1=h5py.File(options.outfile, 'a')
+
+        for inputfile in options.inputfilelist[1:]:
+            f2=h5py.File(inputfile, 'r')
+            combine_fmippn_h5_files(f1,f2)
+            f2.close()
+
+        f1.close()
+
+        
 if __name__ == "__main__":
 
     #Parse commandline arguments
